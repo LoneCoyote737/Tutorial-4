983dabff5b2e7e20e0a13bdeb4cfd833a0ce2ea2
Drajuan <sp.john.manuel737@gmail.com>
2024-07-07 18:12:00 +0530

    added sellItem

--- q1.scala
+++ q1.scala
@@ -6,9 +6,10 @@ object Q1 extends App {
     val len = itemNames.length
 
     val stringMaker = (i: Int) => s"${itemNames(i)}\t-\t${itemQuantity(i)}"
-    val displayInventory = (length: Int) => {
+    val displayInventory = () => {
         println("Inventory:")
         for(i <- 0 to len-1) println(stringMaker(i))
+        println("")
     }
 
     val findIndex = (name: String) => itemNames.indexOf(name)
@@ -17,10 +18,20 @@ object Q1 extends App {
         case x => itemQuantity(x) = itemQuantity(x) + quantity
     }
 
-    restockItem("Bag", 3)
-    displayInventory(len)
+    val sellItem = (name: String, quantity: Int) => findIndex(name) match {
+        case -1 => println("Item not found!")
+        case x => itemQuantity(x) = itemQuantity(x) - quantity
+    }
 
+    // testing code:
+
+    restockItem("Bag", 3)
+    displayInventory()
     restockItem("Paper", 3)
-    displayInventory(len)
-    
+    displayInventory()
+
+    sellItem("Bag", 2)
+    displayInventory()
+    sellItem("Pen", 2)
+    displayInventory()
 }
